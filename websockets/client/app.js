192c3508f8cf283ecf92935830e06bdc4e80104d
Razvan G <razvan.goldstein@gmail.com>
2018-11-09 10:44:10 +0200

    removed socket.io dependencies

--- websockets/client/app.js
+++ websockets/client/app.js
@@ -1,25 +1,12 @@
 const WebSocket = require('ws')
-const connection = new WebSocket('ws://localhost:3000')
+const connection = new WebSocket('ws://localhost:8080')
 
 connection.on('open', (data)=>{
-    console.log('connected')
-    connection.send('data')
+    console.log('----- Connected to Server -------')
+    connection.send('message')
 })
 
 connection.on('message', (data)=>{
     console.log(JSON.parse(data))
-    //connection.send('data')
 })
 
-// var io = require('socket.io-client')
-// var socket = io.connect('http://localhost:3000', {reconnect:true})
-
-// socket.on('connect', function (socket) {
-//     console.log('Connected!');
-// });
-
-// socket.on('news', function(data){
-//     console.log(data)
-// })
- 
-// socket.emit('event', {data:true});
--- websockets/server/app.js
+++ websockets/server/app.js
@@ -1,87 +1,26 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator')
-var cors = require('cors')
-
 var testdata = require('./utils/testdata')
 
-var api = require('./routes/api');
+const WebSocket = require('ws');
 
-var app = express();
+const wss = new WebSocket.Server({ port: 8080 });
 
-// var server = require('http').Server(app)
-// var io = require('socket.io')(server)
-
-// server.listen(3000)
-
-var WebSocket = require('ws');
-
-var wss = new WebSocket.Server({ port: 3000 });
-
-wss.on('connection', function connection(ws) {
-    // Broadcast to everyone else.
-    setInterval(function timeout() {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(testdata));
-            }
-          });
-      }, 10000);
+// wss.on('connection', function connection(ws) {
+//     // Broadcast to everyone else.
+//     setInterval(function timeout() {
+//         wss.clients.forEach(function each(client) {
+//             if (client.readyState === WebSocket.OPEN) {
+//                 client.send(JSON.stringify(testdata));
+//             }
+//           });
+//       }, 10000);
     
-});
+// });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator())
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
-
-app.options('*', cors())
-
-
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html');
+//for testing purposes only
+wss.on('connection', function connection(ws){
+    ws.on('message', function incoming(message){
+        ws.send(JSON.stringify(testdata))
+    })
 })
 
-// io.on('connection', function (socket) {
-//     socket.emit('news', { hello: 'world' });
-//     socket.on('event', function (data) {
-//       console.log(data);
-//     });
-//   });
-   
-
-app.use( '/api/v1', api)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send({errors:[{msg:'An error occured'}]});
-});
-
-module.exports = app;
 
