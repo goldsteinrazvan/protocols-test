2e858b96c653f8b02fa8c9c48d5430970aa1d240
Razvan G <razvan.goldstein@gmail.com>
2018-11-09 16:21:07 +0200

    updated generator util

--- protobuf/server/utils/generator.js
+++ protobuf/server/utils/generator.js
@@ -2,24 +2,28 @@ var faker = require('faker')
 
 function generateJSON(){
     var data = {}
-    data.id = faker.random.number()
-    data.firstName = faker.name.firstName()
-    data.lastName = faker.name.lastName()
-    data.specialUser = faker.random.boolean()
-    data.userCredit = null
-    data.userImages = new Array(5).fill(null).map( (e) => e = faker.image.image())
-    data.settings = { 
-        avatar: faker.internet.avatar(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    }
-    data.orders = new Array(10).fill(null).map((e) =>{
+    data.id = faker.random.uuid()
+    data.index = faker.random.number()
+    data.guid = faker.random.uuid()
+    data.balance = faker.finance.currencySymbol() + faker.finance.amount()
+    data.image = faker.image.avatar()
+    data.age = faker.random.number(90)
+    data.eyeColor = faker.commerce.color()
+    data.name = faker.name.findName()
+    data.gender = faker.random.arrayElement(['female', 'male'])
+    data.company = faker.company.companyName()
+    data.email = faker.internet.email()
+    data.phone = faker.phone.phoneNumber()
+    data.address = faker.address.streetAddress()
+    data.about = faker.lorem.paragraph(3)
+    data.registered = faker.date.past()
+    data.latitude = faker.address.latitude()
+    data.longitude = faker.address.longitude()
+    data.tags = new Array(7).fill(null).map( (e) => e = faker.lorem.word())
+    data.friends = new Array(3).fill(null).map((e) =>{
       return {
-            order_id: faker.random.number(),
-            placed: faker.date.past(),
-            amount: faker.finance.amount(),
-            currency: faker.finance.currencyCode(),
-            payment_received: faker.random.boolean()
+            id: faker.random.number(),
+            name: faker.name.findName()
         }   
     })
     return data;
