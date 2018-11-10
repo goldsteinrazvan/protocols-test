85644e043470f2d1410c97e1735be68dae3df7a2
Razvan G <razvan.goldstein@gmail.com>
2018-11-10 19:39:56 +0200

    removed unused dependencies

--- protobuf/server/app.js
+++ protobuf/server/app.js
@@ -15,11 +15,6 @@ protobuf.load('./proto/testmessage.proto', (err, root) => {
     TestMessage = root.lookupType('TestMessage')
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
