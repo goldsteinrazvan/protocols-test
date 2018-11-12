65e90445a2e551906c6bb7cde17f0a44a244df45
Razvan G <razvan.goldstein@gmail.com>
2018-11-12 12:24:09 +0200

    log notification when message is sent to client

--- websockets/server/app.js
+++ websockets/server/app.js
@@ -4,22 +4,32 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+var sentMessages = 0;
+
 wss.on('connection', function connection(ws) {
-    // Broadcast to everyone else.
+    // Broadcast to clients every 10 seconds
     setInterval(function timeout() {
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify(testdata));
+                messageSent();
             }
           });
       }, 10000);
     
 });
 
-//for testing purposes only
+//send message when client connects
 wss.on('connection', function connection(ws){
     ws.on('message', function incoming(message){
         ws.send(JSON.stringify(testdata))
+        messageSent();
     })
 })
 
+//increase the amount of sent messages and log the amount
+function messageSent(){
+    sentMessages++;
+    console.log(" >>>> Message sent <<<<")
+    console.log(" Number of sent messages: " + sentMessages)
+}
