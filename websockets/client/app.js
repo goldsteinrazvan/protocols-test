3088bee4b4ce3c1d5dda9fcf022d23bf1881651d
Razvan G <razvan.goldstein@gmail.com>
2018-11-12 12:24:23 +0200

    added comments to code

--- websockets/client/app.js
+++ websockets/client/app.js
@@ -1,11 +1,13 @@
 const WebSocket = require('ws')
 const connection = new WebSocket('ws://localhost:8080')
 
+//send a message when connected to server
 connection.on('open', (data)=>{
     console.log('----- Connected to Server -------')
     connection.send(data)
 })
 
+//receive incoming messages and log them
 connection.on('message', (data)=>{
     console.log(JSON.parse(data))
 })
